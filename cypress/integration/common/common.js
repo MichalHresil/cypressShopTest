533d884b0839cc4245ebd897598b496ad9352b29
Hřešil Michal <hresil@cngroup.dk>
2022-07-25 11:21:07 +0200

    optimized code
    removed the switch created when having a brain dead moment

--- cypress/integration/common/common.js
+++ cypress/integration/common/common.js
@@ -5,7 +5,7 @@
 // =============================================================================
 
 // Imports =====================================================================
-import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps"
+import { defineStep } from "cypress-cucumber-preprocessor/steps"
 import CartPage from "../../support/pageClasses/CartPage"
 import HomePage from "../../support/pageClasses/HomePage"
 
@@ -13,108 +13,46 @@ import HomePage from "../../support/pageClasses/HomePage"
 const homepage = new HomePage()
 const cartpage = new CartPage()
 
+
 // =============================================================================
 // SHARED STEP DEFINITIONS
 // =============================================================================
 
 
 // Visiting page ===============================================================
-Given('I navigate to {string} page', function (pageStr) {
+defineStep('I navigate to {string} page', function (pageStr) {
     cy.fixture("URL").then(function (url) {
         this.url = url
-        switch (pageStr.toLowerCase()) {
-            case "main":
-                cy.visit(this.url.main)
-                break
-            case "store":
-                cy.visit(this.url.store)
-                break
-            case "cart":
-                cy.visit(this.url.cart)
-                break
-            case "checkout":
-                cy.visit(this.url.checkout)
-                break
-            case "accessories":
-                cy.visit(this.url.accessories)
-                break
-            case "men":
-                cy.visit(this.url.men)
-                break
-            case "women":
-                cy.visit(this.url.women)
-                break
-            case "about":
-                cy.visit(this.url.about)
-                break
-            case "contact":
-                cy.visit(this.url.contact)
-                break
-            case "account":
-                cy.visit(this.url.account)
-                break
-
-        }
+        cy.visit(this.url[pageStr.toLowerCase()])
     })
 
 })
 
 // Loading data ================================================================
-And('I load data {string}', function (data) {
+defineStep('I load data {string}', function (data) {
     cy.fixture(data).then(function (obj) {
         this.items = obj.items
     })
 })
 
 // Should be on page ===========================================================
-Then('I should be on {string} page', function (pageStr) {
-    switch (pageStr) {
-        case "main":
-            cy.url().should("deep.equal", this.url.main)
-            break
-        case "store":
-            cy.url().should("deep.equal", this.url.store)
-            break
-        case "cart":
-            cy.url().should("deep.equal", this.url.cart)
-            break
-        case "checkout":
-            cy.url().should("deep.equal", this.url.checkout)
-            break
-        case "accessories":
-            cy.url().should("deep.equal", this.url.accessories)
-            break
-        case "men":
-            cy.url().should("deep.equal", this.url.men)
-            break
-        case "women":
-            cy.url().should("deep.equal", this.url.women)
-            break
-        case "about":
-            cy.url().should("deep.equal", this.url.about)
-            break
-        case "contact":
-            cy.url().should("deep.equal", this.url.contact)
-            break
-        case "account":
-            cy.url().should("deep.equal", this.url.account)
-            break
-    }
+defineStep('I should be on {string} page', function (pageStr) {
+    cy.url().should("deep.equal", this.url[pageStr.toLowerCase()])
 })
 
 // Put one item to cart ========================================================
-And('I put one item to cart', function () {
+defineStep('I put one item to cart', function () {
     homepage.addItemToCartHomepage(this.items[0].name, this.items[0].quantity)
 })
 
 // Validate item presence ======================================================
-When('I validate the item is present in cart', function () {
+defineStep('I validate the item is present in cart', function () {
     homepage.getCartButtonNavBar().click({ force: true })
     cartpage.getAllSubPricesFromCart().should("have.length", 1)
 })
 
 // Click on proceed to checkout ================================================
-And('I click on proceed to checkout button', function () {
+defineStep('I click on proceed to checkout button', function () {
     cartpage.getCheckoutButton().click({ force: true })
 })
 
